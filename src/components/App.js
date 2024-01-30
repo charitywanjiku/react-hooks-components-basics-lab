1ff185073414616d0e8d3b99378d52abe2dae161
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-01-30 11:40:45 +0300

    Assignment Done

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,13 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 {/* write an <About> component here */}
 
@@ -23,6 +30,8 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Home></Home>
+      <About></About>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
